25aa94ebddb80a16f642b35df4c88d03456425fe
максим пашута <max.pashuta@mail.ru>
2020-07-11 15:30:17 +0300

    Added comments to task 1

--- EPAM_Task2/Task1/ThreeDimensionalVector.cs
+++ EPAM_Task2/Task1/ThreeDimensionalVector.cs
@@ -29,11 +29,11 @@ namespace EPAM_Task2.Task1
         public int Z { get; set; }
 
         /// <summary>
-        /// 
+        /// Addition of vectors by adding the corresponding coordinates.
         /// </summary>
-        /// <param name="threeDemVector1"></param>
-        /// <param name="threeDemVector2"></param>
-        /// <returns></returns>
+        /// <param name="threeDemVector1">First three-dimensional vector</param>
+        /// <param name="threeDemVector2">Second three-dimensional vector</param>
+        /// <returns>Three-dimensional vector</returns>
         public static ThreeDimensionalVector operator +(ThreeDimensionalVector threeDemVector1, ThreeDimensionalVector threeDemVector2)
         {
             return new ThreeDimensionalVector(threeDemVector1.X + threeDemVector2.X,
@@ -43,11 +43,11 @@ namespace EPAM_Task2.Task1
 
 
         /// <summary>
-        /// 
+        /// The difference of vectors by subtracting the corresponding coordinates.
         /// </summary>
-        /// <param name="threeDemVector1"></param>
-        /// <param name="threeDemVector2"></param>
-        /// <returns></returns>
+        /// <param name="threeDemVector1">First three-dimensional vector</param>
+        /// <param name="threeDemVector2">Second three-dimensional vector</param>
+        /// <returns>Three-dimensional vector</returns>
         public static ThreeDimensionalVector operator -(ThreeDimensionalVector threeDemVector1, ThreeDimensionalVector threeDemVector2)
         {
             return new ThreeDimensionalVector(threeDemVector1.X - threeDemVector2.X,
@@ -56,11 +56,11 @@ namespace EPAM_Task2.Task1
         }
 
         /// <summary>
-        /// 
+        /// Multiplying a vector by a number by multiplying the number by the coordinates.
         /// </summary>
-        /// <param name="threeDemVector1"></param>
-        /// <param name="number"></param>
-        /// <returns></returns>
+        /// <param name="threeDemVector1">Three-dimensional vector</param>
+        /// <param name="number">Any number</param>
+        /// <returns>Three-dimensional vector</returns>
         public static ThreeDimensionalVector operator *(ThreeDimensionalVector threeDemVector1, int number)
         {
             return new ThreeDimensionalVector(threeDemVector1.X * number,
@@ -69,22 +69,22 @@ namespace EPAM_Task2.Task1
         }
 
         /// <summary>
-        /// 
+        /// Multiplying a number by a vector by multiplying the number by the coordinates.
         /// </summary>
-        /// <param name="number"></param>
-        /// <param name="threeDemVector1"></param>
-        /// <returns></returns>
+        /// <param name="number">Any number</param>
+        /// <param name="threeDemVector1">Three-dimensional vector</param>
+        /// <returns>Three-dimensional vector</returns>
         public static ThreeDimensionalVector operator *(int number, ThreeDimensionalVector threeDemVector1)
         {
             return threeDemVector1 * number;
         }
 
         /// <summary>
-        /// 
+        /// Dividing a vector by a number by dividing each coordinate by a number.
         /// </summary>
-        /// <param name="threeDemVector1"></param>
-        /// <param name="number"></param>
-        /// <returns></returns>
+        /// <param name="threeDemVector1">Three-dimensional vector</param>
+        /// <param name="number">Any number</param>
+        /// <returns>Three-dimensional vector</returns>
         public static ThreeDimensionalVector operator /(ThreeDimensionalVector threeDemVector1, int number)
         {
             if (number == 0)
@@ -98,11 +98,11 @@ namespace EPAM_Task2.Task1
         }
 
         /// <summary>
-        /// 
+        /// Scalar multiplication of vectors by adding the products of the corresponding coordinates.
         /// </summary>
-        /// <param name="threeDemVector1"></param>
-        /// <param name="threeDemVector2"></param>
-        /// <returns></returns>
+        /// <param name="threeDemVector1">First three-dimensional vector</param>
+        /// <param name="threeDemVector2">Second three-dimensional vector</param>
+        /// <returns>Any number</returns>
         public static int operator *(ThreeDimensionalVector threeDemVector1, ThreeDimensionalVector threeDemVector2)
         {
             return threeDemVector1.X * threeDemVector2.X +
@@ -111,11 +111,11 @@ namespace EPAM_Task2.Task1
         }
 
         /// <summary>
-        /// 
+        /// Vector multiplication of vectors.
         /// </summary>
-        /// <param name="threeDemVector1"></param>
-        /// <param name="threeDemVector2"></param>
-        /// <returns></returns>
+        /// <param name="threeDemVector1">First three-dimensional vector</param>
+        /// <param name="threeDemVector2">Second three-dimensional vector</param>
+        /// <returns>Three-dimensional vector</returns>
         public static ThreeDimensionalVector CrossVectors(ThreeDimensionalVector threeDemVector1, ThreeDimensionalVector threeDemVector2)
         {
             return new ThreeDimensionalVector(threeDemVector1.Y * threeDemVector2.Z - threeDemVector1.Z * threeDemVector2.Y,
@@ -124,10 +124,10 @@ namespace EPAM_Task2.Task1
         }
 
         /// <summary>
-        /// 
+        /// Method for equal the current object with the specified object.
         /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
+        /// <param name="obj">Any object</param>
+        /// <returns>True or False</returns>
         public override bool Equals(object obj)
         {
             if (!(obj is ThreeDimensionalVector))
@@ -143,18 +143,18 @@ namespace EPAM_Task2.Task1
         }
 
         /// <summary>
-        /// 
+        /// The method calculates the hash code for the current object.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Hash code</returns>
         public override int GetHashCode()
         {
             return (X.GetHashCode() + Y.GetHashCode() + Z.GetHashCode()) * 3;
         }
 
         /// <summary>
-        /// 
+        /// The method creates and returns a string representation of the object.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>String representation</returns>
         public override string ToString()
         {
             return string.Format($"({X}; {Y}; {Z})");
